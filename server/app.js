e509ec648bcfe5cc9cb54f0ed5a85031568331a7
Inyeol.hong <inyeol.hong@2bytescorp.com>
2024-05-02 22:31:09 +0900

    fix: employees에 타이틀 추가

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 //mariaDB
 const mysql = require('mysql2');
+const { getPingPong, getEmployees } = require('./controller');
 
 const app = express();
 const port = 3000;
@@ -35,30 +36,62 @@ connection.connect((err) => {
 //   next();
 // });
 
-app.get('/', (req, res) => {
-  const timeout = setTimeout(() => {
-    if (req.query.text === 'Ping') {
-      res.send('Pong');
-      return;
-    }
-    res.send(req.query.text);
-  }, 3000);
+//문제2 용
+app.get('/', getPingPong);
 
-  // timeoutArray.push(timeout);
-});
-
+//문제3 용
 app.get('/employees', (req, res) => {
-  console.log('employees');
-  connection.query('SELECT * FROM employees', (error, results) => {
+  connection.query('SELECT * FROM employees WHERE hire_date >= "2000-01-01"', (error, results) => {
     if (error) {
       console.error('Error:', error);
       res.status(500).send('Internal Server Error');
       return;
     }
-    console.log('results:', results[0]);
-    res.send(JSON.stringify(results[0]));
+
+    console.log('results:', results);
+    //병령 비동기 실행
+    const promises = results.map((result) => {
+      return new Promise((resolve, reject) => {
+        // emp_no를 사용하여 titles 테이블에서 title가좀
+        connection.query('SELECT title FROM titles WHERE emp_no = ?', [result.emp_no], (error, titleResults) => {
+          if (error) {
+            return reject(error);
+          }
+
+          // title 추가
+          result.title = titleResults[0].title;
+          resolve(result);
+        });
+      });
+    });
+
+    Promise.all(promises)
+      .then((resultsWithTitles) => {
+        console.log('resultsWithTitles:', resultsWithTitles);
+        res.send(JSON.stringify(resultsWithTitles));
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+      });
   });
 });
+
+//!! employees 테이블 데이터
+//  emp_no: 종업원번호
+//  last_name: 이름
+//  first_name: 성
+//  gender: 성별,
+//  hire_date: 고용일자
+
+//필요 dept_name 부서이름
+//필요 title 직급
+//필요 max_salary 최대급여
+//  birth_date: 생일 , 불필요
+
+//todo emp_no를 가지고 titles 테이블에서 title을 가져온다.
+//todo emp_no를 가지고 salaries 테이블에서 max_salary를 가져온다.
+//todo emp_no를 가지고 dept_emp 테이블에서 dept_no를 가져온다. -> dept_no를 가지고 dept_name을 가져온다.
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ server/controller/index.js
@@ -0,0 +1,15 @@
+const { connection } = require('../db');
+
+const getPingPong = (req, res) => {
+  const timeout = setTimeout(() => {
+    if (req.query.text === 'Ping') {
+      res.send('Pong');
+      return;
+    }
+    res.send(req.query.text);
+  }, 3000);
+};
+
+module.exports = {
+  getPingPong,
+};
